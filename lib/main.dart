6ff96bf84ebd2659401f21a9dad28f93d416e62b
KyleKun <caiofariapedroso@outlook.com>
2019-06-12 20:55:31 -0300

    added image

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,68 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        //appBar: AppBar(title: Text('My Info')),
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/caio.jpg'),
+              ),
+              Text('Caio Pedroso',
+                  style: TextStyle(
+                    fontFamily: 'Montserrat',
+                    fontSize: 32.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  )),
+              Text('FLUTTER DEVELOPER',
+                  style: TextStyle(
+                    fontFamily: 'Montserrat_2',
+                    fontSize: 22.0,
+                    color: Colors.white70,
+                    letterSpacing: 2.0,
+                  )),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(Icons.phone, color: Colors.teal),
+                    SizedBox(width: 10.0),
+                    Text(
+                      '+55 123 456 789',
+                      style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: 'Montserrat_2',
+                          fontSize: 15.0),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                padding: EdgeInsets.all(10.0),
+                child: (Row(
+                  children: <Widget>[
+                    Icon(Icons.mail, color: Colors.teal),
+                    SizedBox(width: 10.0),
+                    Text(
+                      'example@example.com',
+                      style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: 'Montserrat_2',
+                          fontSize: 15.0),
+                    ),
+                  ],
+                )),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
